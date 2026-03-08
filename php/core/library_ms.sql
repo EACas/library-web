3fc1cdbed90fc28c0ec1c1b80e5e7765728d77fd
Luis Mattu <imadriandelano@gmail.com>
2026-03-07 21:38:31 -0600

    Uploaded DB File

--- php/core/library_ms.sql
+++ php/core/library_ms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2026 at 05:00 AM
+-- Generation Time: Mar 08, 2026 at 04:36 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,6 +32,15 @@ CREATE TABLE `account_status` (
   `account_status` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `account_status`
+--
+
+INSERT INTO `account_status` (`account_status_id`, `account_status`) VALUES
+(1, 'active'),
+(2, 'inactive'),
+(3, 'Suspended');
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +51,18 @@ CREATE TABLE `authors` (
   `author_id` int(11) NOT NULL,
   `author_name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `author_name`) VALUES
+(1, 'J.K. Rowling'),
+(2, 'George Orwell'),
+(3, 'J.R.R. Tolkien'),
+(4, 'Agatha Christie'),
+(5, 'test'),
+(6, 'test3');
 
 -- --------------------------------------------------------
 
@@ -57,6 +78,19 @@ CREATE TABLE `books` (
   `price` float NOT NULL,
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `book_title`, `description`, `publish_date`, `price`, `stock`) VALUES
+(1, 'Harry Potter and the Philosopher\'s Stone', 'Fantasy novel', '1997-06-26', 15.99, 10),
+(2, '1984', 'Dystopian novel', '1949-06-08', 12.5, 7),
+(3, 'The Hobbit', 'Fantasy adventure', '1937-09-21', 14.75, 5),
+(4, 'Murder on the Orient Express', 'Detective novel', '1934-01-01', 11.2, 6),
+(5, 'test', 'sdasd', '2026-03-02', 23, 1),
+(6, 'test', 'asdadasdsqadas', '2026-03-02', 1, 2),
+(8, 'test', 'qweqweqe', '2026-03-02', 132, 3);
 
 -- --------------------------------------------------------
 
@@ -83,6 +117,19 @@ CREATE TABLE `book_authors` (
   `author_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `book_authors`
+--
+
+INSERT INTO `book_authors` (`book_author_id`, `book_id`, `author_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4),
+(5, 5, 4),
+(6, 6, 1),
+(8, 8, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -93,6 +140,16 @@ CREATE TABLE `book_conditions` (
   `book_condition_id` int(11) NOT NULL,
   `book_condition` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `book_conditions`
+--
+
+INSERT INTO `book_conditions` (`book_condition_id`, `book_condition`) VALUES
+(1, 'New'),
+(2, 'Good'),
+(3, 'Used'),
+(4, 'Damaged');
 
 -- --------------------------------------------------------
 
@@ -119,6 +176,24 @@ CREATE TABLE `book_genres` (
   `genre_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `book_genres`
+--
+
+INSERT INTO `book_genres` (`book_genre_id`, `book_id`, `genre_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 2, 3),
+(4, 3, 1),
+(5, 4, 4),
+(6, 4, 5),
+(7, 5, 5),
+(8, 6, 5),
+(9, 8, 5),
+(10, 8, 2),
+(11, 8, 4),
+(12, 8, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -143,6 +218,19 @@ CREATE TABLE `days` (
   `day` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `days`
+--
+
+INSERT INTO `days` (`day_id`, `day`) VALUES
+(1, 'Monday'),
+(2, 'Tuesday'),
+(3, 'Wednesday'),
+(4, 'Thursday'),
+(5, 'Friday'),
+(6, 'Saturday'),
+(7, 'Sunday');
+
 -- --------------------------------------------------------
 
 --
@@ -153,6 +241,18 @@ CREATE TABLE `districts` (
   `district_id` int(11) NOT NULL,
   `district` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `districts`
+--
+
+INSERT INTO `districts` (`district_id`, `district`) VALUES
+(1, 'Corozal'),
+(2, 'Orange Walk'),
+(3, 'Belize'),
+(4, 'Cayo'),
+(5, 'Stann Creek'),
+(6, 'Toledo');
 
 -- --------------------------------------------------------
 
@@ -166,6 +266,16 @@ CREATE TABLE `district_villages` (
   `village` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `district_villages`
+--
+
+INSERT INTO `district_villages` (`district_village_id`, `district`, `village`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 3),
+(4, 3, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -176,6 +286,39 @@ CREATE TABLE `from_days` (
   `from_day_id` int(11) NOT NULL,
   `day_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `from_days`
+--
+
+INSERT INTO `from_days` (`from_day_id`, `day_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genre_id` int(11) NOT NULL,
+  `genre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`genre_id`, `genre`) VALUES
+(1, 'Fantasy'),
+(2, 'Dystopian'),
+(3, 'Science Fiction'),
+(4, 'Mystery'),
+(5, 'Detective');
 
 -- --------------------------------------------------------
 
@@ -208,6 +351,13 @@ CREATE TABLE `library` (
   `is_open` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `library`
+--
+
+INSERT INTO `library` (`library_id`, `library_name`, `district_village`, `phone`, `email`, `allowed_borrowed_books`, `rate_day`, `is_open`) VALUES
+(1, 'Corozal Library', 1, '21234567', 'central@library.com', 5, 0.5, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -223,6 +373,13 @@ CREATE TABLE `opening_hours` (
   `end_hour` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `opening_hours`
+--
+
+INSERT INTO `opening_hours` (`opening_hour_id`, `library_id`, `from_day`, `to_day`, `start_hour`, `end_hour`) VALUES
+(1, 1, 1, 1, 8, 17);
+
 -- --------------------------------------------------------
 
 --
@@ -234,6 +391,15 @@ CREATE TABLE `roles` (
   `role` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role`) VALUES
+(1, 'admin'),
+(2, 'librarian'),
+(3, 'student');
+
 -- --------------------------------------------------------
 
 --
@@ -244,6 +410,16 @@ CREATE TABLE `status` (
   `status_id` int(11) NOT NULL,
   `satus` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`status_id`, `satus`) VALUES
+(1, 'Borrowed'),
+(2, 'Returned'),
+(3, 'Late'),
+(4, 'Reserved');
 
 -- --------------------------------------------------------
 
@@ -269,6 +445,13 @@ CREATE TABLE `to_day` (
   `day_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Dumping data for table `to_day`
+--
+
+INSERT INTO `to_day` (`to_day_id`, `day_id`) VALUES
+(1, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -283,12 +466,23 @@ CREATE TABLE `users` (
   `l_name` varchar(45) NOT NULL,
   `phone_number` varchar(45) NOT NULL,
   `gender` tinyint(4) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` text NOT NULL,
   `dob` datetime NOT NULL,
   `account_status` int(11) NOT NULL,
   `library_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL
+  `role_id` int(11) NOT NULL,
+  `date_added` date NOT NULL DEFAULT curdate()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_library_id`, `email`, `f_name`, `l_name`, `phone_number`, `gender`, `password`, `dob`, `account_status`, `library_id`, `role_id`, `date_added`) VALUES
+(5, 'LIB69ac85c0dc536', 'admin@test.com', 'Luis', 'Mattu', '6264246', 1, '$2y$10$Wa9jZ6LEj4vyqgFx5/Ami.fU1K4RKLT3yk0y.J0A33ksffhpnq8SO', '2026-03-07 00:00:00', 1, 1, 1, '2026-03-07'),
+(6, 'LIB69ac86168ea68', 'librarian@test.com', 'Esmiri', 'Castillo', '6264246', 2, '$2y$10$Okog5CfTlgq7NBIgAmBw/e/2/tGfhzeGAjLkd7v.2JJhOh4FzLR6K', '2026-03-07 00:00:00', 1, 1, 2, '2026-03-07'),
+(7, 'LIB69ac864a2908d', 'student@test.com', 'Madelyn', 'Cunil', '6264246', 2, '$2y$10$YCD/S9iVC1rODbQMOHHUGes2PbVxtU00ssuOGhu6vO1nOHqF34mqC', '2026-03-07 00:00:00', 1, 1, 3, '2026-03-07'),
+(9, 'LIB69acd9b7166dc', 'student2@gmail.com', 'Daniel', 'Villanueva', '6264246', 1, '$2y$10$KpIhaqZy8HOOpyrqJgAqw.BT9Xh5/KGMgIn2CTsXNkfToZcB0UhZO', '0000-00-00 00:00:00', 1, 1, 3, '2026-03-07');
 
 -- --------------------------------------------------------
 
@@ -343,6 +537,16 @@ CREATE TABLE `villages` (
   `village_id` int(11) NOT NULL,
   `village` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `villages`
+--
+
+INSERT INTO `villages` (`village_id`, `village`) VALUES
+(1, 'Ranchito'),
+(2, 'Santa Elena'),
+(3, 'San Roman'),
+(4, 'Punta Gorda');
 
 --
 -- Indexes for dumped tables
@@ -441,6 +645,12 @@ ALTER TABLE `from_days`
   ADD KEY `fk_from_days1_idx` (`day_id`);
 
 --
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
 -- Indexes for table `librarians`
 --
 ALTER TABLE `librarians`
@@ -535,19 +745,19 @@ ALTER TABLE `villages`
 -- AUTO_INCREMENT for table `account_status`
 --
 ALTER TABLE `account_status`
-  MODIFY `account_status_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `account_status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `book_actions_books`
@@ -559,13 +769,13 @@ ALTER TABLE `book_actions_books`
 -- AUTO_INCREMENT for table `book_authors`
 --
 ALTER TABLE `book_authors`
-  MODIFY `book_author_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `book_conditions`
 --
 ALTER TABLE `book_conditions`
-  MODIFY `book_condition_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_condition_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `book_condition_library_rate`
@@ -577,7 +787,7 @@ ALTER TABLE `book_condition_library_rate`
 -- AUTO_INCREMENT for table `book_genres`
 --
 ALTER TABLE `book_genres`
-  MODIFY `book_genre_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `book_reservation_books`
@@ -589,25 +799,31 @@ ALTER TABLE `book_reservation_books`
 -- AUTO_INCREMENT for table `days`
 --
 ALTER TABLE `days`
-  MODIFY `day_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `day_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `districts`
 --
 ALTER TABLE `districts`
-  MODIFY `district_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `district_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `district_villages`
 --
 ALTER TABLE `district_villages`
-  MODIFY `district_village_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `district_village_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `from_days`
 --
 ALTER TABLE `from_days`
-  MODIFY `from_day_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `from_day_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `librarians`
@@ -619,13 +835,19 @@ ALTER TABLE `librarians`
 -- AUTO_INCREMENT for table `library`
 --
 ALTER TABLE `library`
-  MODIFY `library_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `library_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `opening_hours`
 --
 ALTER TABLE `opening_hours`
-  MODIFY `opening_hour_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `opening_hour_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `students`
@@ -637,13 +859,13 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `to_day`
 --
 ALTER TABLE `to_day`
-  MODIFY `to_day_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `to_day_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_book_actions`
@@ -667,7 +889,7 @@ ALTER TABLE `user_district_village`
 -- AUTO_INCREMENT for table `villages`
 --
 ALTER TABLE `villages`
-  MODIFY `village_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `village_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
